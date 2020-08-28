4625099b18d83eb2cac064484ce81c3dc6632d19
Theo Weeden <theo.weeden@gmail.com>
2020-08-28 22:25:06 +0100

    Set up basic classes needed for checkers implementation

---
+++ TicTacToe/TicTacToe/CheckersImplementation/Checkers.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Checkers : IGame
+    {
+        public string[,] Board { get; set; }
+        public const int BoardSize = 8;
+        public const int heuristicMultiplier = 100;
+
+        public Checkers()
+        {
+            Board = initBoard();
+        }
+
+        public string[,] initBoard()
+        {
+            var board = new string[BoardSize, BoardSize];
+
+            for (int i = 0; i < BoardSize; i++)
+            {
+                for (int j = 0; j < BoardSize; j++)
+                {
+                    board[i, j] = " ";
+                }
+            }
+
+            return board;
+        }
+
+        public bool IsWon()
+        {
+            return IsWon('B') || IsWon('W');
+        }
+        public bool IsWon(char player)
+        {
+            return false;//TODO Implement
+        }
+
+        public int Evaluate(char player)
+        {
+            return 0;//TODO Implement
+        }
+
+        public char NextPlayer(char player)
+        {
+            return player switch
+            {
+                'B' => 'W',
+                'W' => 'B',
+                _ => ' ',
+            };
+        }
+
+        public List<IMove> GetValidMoves(char player)
+        {
+            var moves = new List<IMove>();
+            
+            //TODO Implement
+
+            return moves;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (int y = 0; y < BoardSize; y++)
+            {
+                sb.Append("|");
+                for (int x = 0; x < BoardSize; x++)
+                {
+                    sb.Append(Board[x, y] + "|");
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+    }
+}
+
---
+++ TicTacToe/TicTacToe/CheckersImplementation/CheckersMove.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class CheckersMove : IMove
+    {
+        public Coords From { get; set; }
+        public Coords To { get; set; }
+        public char Player { get; set; }
+
+        public CheckersMove(Coords from,Coords to,char player)
+        {
+            From = from;
+            To = to;
+            Player = player;
+        }
+
+        public bool Execute(IGame game)
+        {
+            if (!(game is Checkers) || !IsValid(game)) return false;
+
+            var checkers = game as Checkers;
+
+            checkers.Board[To.x, To.y] = checkers.Board[From.x, From.y];
+            checkers.Board[From.x, From.y] = " ";
+            return true;
+        }
+        public void Undo(IGame game)
+        {
+            if (!(game is Checkers)) return;
+            var checkers = game as Checkers;
+
+            checkers.Board[From.x, From.y] = checkers.Board[To.x, To.y];
+            checkers.Board[To.x, To.y] = " ";
+        }
+        public bool IsValid(IGame game)
+        {
+            if (!(game is Checkers)) return false;
+            var checkers = game as Checkers;
+            if ((checkers.IsWon('B') || checkers.IsWon('W')
+                || From == null || From.x >= TicTacToe.BoardSize || From.y >= TicTacToe.BoardSize
+                || To == null || To.x >= TicTacToe.BoardSize || To.y >= TicTacToe.BoardSize
+                || !checkers.Board[From.x, From.y].Contains(Player)
+                || checkers.Board[From.x, From.y] == " " || checkers.Board[To.x, To.y] != " ")) return false;
+         
+            
+            return true;
+        }
+    }
+}
