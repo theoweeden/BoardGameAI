55d6c501dfd078316dc7d835c4df72fc50353738
Theo Weeden <theo.weeden@gmail.com>
2020-08-30 22:22:44 +0100

    Added some proper validation logic and the ability to take pieces

--- TicTacToe/TicTacToe/CheckersImplementation/CheckersMove.cs
+++ TicTacToe/TicTacToe/CheckersImplementation/CheckersMove.cs
@@ -25,6 +25,10 @@ namespace TicTacToe
 
             checkers.Board[To.x, To.y] = checkers.Board[From.x, From.y];
             checkers.Board[From.x, From.y] = " ";
+
+            if (Math.Abs(To.x - From.x) == 2 && Math.Abs(To.y - From.y) == 2) 
+                checkers.Board[(To.x + From.x) / 2, (To.y + From.y) / 2] = " ";
+
             return true;
         }
         public void Undo(IGame game)
@@ -34,6 +38,9 @@ namespace TicTacToe
 
             checkers.Board[From.x, From.y] = checkers.Board[To.x, To.y];
             checkers.Board[To.x, To.y] = " ";
+
+            if (Math.Abs(To.x - From.x) == 2 && Math.Abs(To.y - From.y) == 2)
+                checkers.Board[(To.x + From.x) / 2, (To.y + From.y) / 2] = checkers.NextPlayer(Player).ToString();//TODO Reimplement
         }
         public bool IsValid(IGame game)
         {
@@ -45,7 +52,14 @@ namespace TicTacToe
             if (!checkers.Board[From.x, From.y].Contains(Player)
                 || checkers.Board[From.x, From.y] == " " || checkers.Board[To.x, To.y] != " ") return false;
 
-         
+            if (To.x == From.x || To.y == From.y) return false;
+
+            if (Player == Checkers.Player1 && To.y <= From.y) return false;
+            if (Player == Checkers.Player2 && To.y >= From.y) return false;
+
+            if (!((Math.Abs(To.x - From.x) == 1 && Math.Abs(To.y - From.y) == 1)
+                || (Math.Abs(To.x - From.x) == 2 && Math.Abs(To.y - From.y) == 2 && checkers.Board[(To.x + From.x) / 2, (To.y + From.y) / 2].Contains(checkers.NextPlayer(Player))))) return false;
+
             return true;
         }
     }
